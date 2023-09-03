a78545c4da8bb6a0f916a3f11607b25d15ba3278
MikolajHanusz <92816085+MikolajHanusz@users.noreply.github.com>
2023-09-03 13:57:46 +0200

    Add files via upload

---
+++ KlientFTP/Klient.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace KlientFTP
+{
+    internal class Klient
+    {
+        public string Host     { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        private string ftpDir;
+        public string FtpDir
+        {
+            get 
+            {
+                if (ftpDir.StartsWith("ftp://"))
+                    return ftpDir;
+                else
+                    return "ftp://" + ftpDir;
+            }
+            set 
+            { 
+                ftpDir = value; 
+            }
+        }
+
+        public Klient() { }
+        public Klient(string host, string user, string pswd) 
+        {
+            Host = host;
+            UserName = user;
+            Password = pswd;
+            FtpDir = host;
+        }
+
+        public ArrayList GetDirs() 
+        {
+            ArrayList dirs = new ArrayList();
+            FtpWebRequest req;
+            try
+            {
+                req = (FtpWebRequest)WebRequest.Create(FtpDir);
+                req.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                req.Credentials = new NetworkCredential(this.UserName, this.Password);
+                req.KeepAlive = false;
+                using (FtpWebResponse res = (FtpWebResponse)req.GetResponse()) 
+                {
+                    Stream str = res.GetResponseStream();
+                    using (StreamReader sr = new StreamReader(str)) 
+                    {
+                        string directory;
+                        while((directory = sr.ReadLine()) != null) 
+                        {
+                            dirs.Add(directory);
+                        }
+                    }
+                }
+                return dirs;
+            }
+            catch (Exception e) 
+            {
+                throw new Exception("Connection error: "+ Host, e);
+            }
+        }
+
+        public ArrayList ChangeDir(string dir)
+        {
+            ftpDir += "/" + dir;
+            return GetDirs();
+        }
+        public ArrayList ChangeDirUp()
+        {
+            if(ftpDir != "ftp://"+Host)
+            {
+                ftpDir = ftpDir.Remove(ftpDir.LastIndexOf("/"), ftpDir.Length - ftpDir.LastIndexOf("/"));
+                return GetDirs();
+            }
+            else 
+                return GetDirs();
+        }
+
+        public void DownloadFile(string ftpName, string localName)
+        {
+            using (WebClient request = new WebClient())
+            {
+                request.Credentials = new NetworkCredential(this.UserName, this.Password);
+                byte[] fileData = request.DownloadData(ftpName);
+                string name = ftpName.Substring(ftpName.LastIndexOf("/"));              
+                using (FileStream file = System.IO.File.Create(localName + name))
+                {
+                    file.Write(fileData, 0, fileData.Length);
+                    file.Close();
+                    System.Windows.MessageBox.Show("File downloaded");
+                }
+            }
+        }
+
+        public async void UploadFile(string ftpName, string localName)
+        {
+
+            string name = ftpName + "/" + Path.GetFileName(localName);
+            var builder = new StringBuilder();
+            int count = 0;
+            int i = 0;
+            foreach (var c in name)
+            {
+                builder.Append(c);
+                if (c == '/') i++;
+                if(i == 3)
+                {
+                    i = 0;
+                    builder.Append('/');
+                }
+            }
+            name = builder.ToString();    
+
+            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(name);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(this.UserName, this.Password);
+            request.UsePassive = true;
+            request.UseBinary = true;
+            request.KeepAlive = true;
+
+            FileStream stream = System.IO.File.OpenRead(localName);
+            byte[] buffer = new byte[stream.Length];
+
+            stream.Read(buffer, 0, buffer.Length);
+            stream.Close();
+
+            Stream reqStream = request.GetRequestStream();
+            reqStream.Write(buffer, 0, buffer.Length);
+            reqStream.Close();
+            System.Windows.MessageBox.Show("File Uploaded");
+        }
+    }
+}
---
+++ KlientFTP/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace KlientFTP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        FolderBrowserDialog dialog = new FolderBrowserDialog();
+        Klient kl = new Klient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtLocalPath.Text = dialog.SelectedPath;
+        }
+
+        private void GetFtpContent(ArrayList dirs)
+        {
+            lbFtpDir.Items.Clear();
+            lbFtpDir.Items.Add("[..]");
+            dirs.Sort();
+            foreach (string dir in dirs) 
+            {
+                string pos=dir.Substring(dir.LastIndexOf(' ') + 1,dir.Length - dir.LastIndexOf(' ')-1);
+                if(pos != ".." && pos != ".")
+                    lbFtpDir.Items.Add(pos);
+            }
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtSerwer.Text !=String.Empty)
+                try
+                {
+                    string serwerName= txtSerwer.Text;
+                    kl = new Klient(serwerName, txtLogin.Text, txtPassword.Password);
+                    GetFtpContent(kl.GetDirs());
+                    txtFtpPath.Text = kl.FtpDir;
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show("Błąd");
+                }
+            else
+            {
+                System.Windows.MessageBox.Show("Server Name");
+                txtSerwer.Text = string.Empty;
+            }
+        }
+
+        private void lbFtpDir_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            int idx = lbFtpDir.SelectedIndex;
+            try
+            {
+                if (idx > -1)
+                {
+                    if (idx == 0)
+                    {
+                        GetFtpContent(kl.ChangeDirUp());
+                        txtFtpPath.Text = kl.FtpDir;
+                    }
+                    else
+                    {
+                        string dir = lbFtpDir.Items[idx].ToString();
+                        if (dir.Contains('.'))
+                        {
+                            txtFtpPath.Text += "/" + dir;
+                            return;
+                        }
+                        GetFtpContent(kl.ChangeDir(dir));
+                        txtFtpPath.Text = kl.FtpDir;
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                System.Windows.MessageBox.Show("Błąd");
+            }
+        }
+
+        private void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            kl.DownloadFile(txtFtpPath.Text, txtLocalPath.Text);
+        }
+
+        private void btnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            txtSerwer.Clear();
+            txtFtpPath.Clear();
+            txtLogin.Clear();
+            txtPassword.Clear();
+            lbFtpDir.Items.Clear();
+        }
+
+        private void btnUpload_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            try
+            {
+                ofd.ShowDialog();
+                kl.UploadFile(txtFtpPath.Text, ofd.FileName);
+            }
+            catch (Exception ex) 
+            {
+                System.Windows.MessageBox.Show("Błąd");
+            }
+        }
+    }
+}
